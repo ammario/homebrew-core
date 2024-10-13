71f0c2f0ec4bdf58ba5d5731cb5bf72b938c8fc1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-10-13 15:44:16 +0000

    ocicl 2.5.2

--- Formula/o/ocicl.rb
+++ Formula/o/ocicl.rb
@@ -1,10 +1,9 @@
 class Ocicl < Formula
   desc "OCI-based ASDF system distribution and management tool for Common Lisp"
   homepage "https://github.com/ocicl/ocicl"
-  url "https://github.com/ocicl/ocicl/archive/refs/tags/v2.4.3.tar.gz"
-  sha256 "722ffe7bc0d2559d758f6ebdc803357c53d0fd47612cc498047aea74ca1a481b"
+  url "https://github.com/ocicl/ocicl/archive/refs/tags/v2.5.2.tar.gz"
+  sha256 "c06e0891c9b3d6e58aabeeaac907a100329645a5f73d34797b585046bd3b9c4f"
   license "MIT"
-  revision 2
 
   bottle do
     sha256 arm64_sequoia: "27c1ae029808d59116becded1c5c3e56ebdc97a59f28efd57990d794ed4eb0d5"
